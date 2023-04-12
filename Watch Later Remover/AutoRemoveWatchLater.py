430ad85baa5d83bcca4a1d3b6b51cfd90069b8cd
LunarChickadee <90534210+LunarChickadee@users.noreply.github.com>
2023-04-12 16:24:58 -0400

    Youtube should have built this for you

--- Watch Later Remover/AutoRemoveWatchLater.py
+++ Watch Later Remover/AutoRemoveWatchLater.py
@@ -1,32 +1,24 @@
 import pyautogui 
 import time
-import keyboard
 import sys
 
     #uncomment the below code to find out where you need to click
     #it'll display the x and y in the terminal for you
     #comment it out again to run the rest of the script
     
-pyautogui.displayMousePosition()
-quit()
-
-#change these numbers to where your menu pops up
-pyautogui.moveTo(1396, 377)
-pyautogui.click()
-pyautogui.click()
-
-#change these numbers to where the remove from watch later option pops up
-pyautogui.moveTo(1280, 500)
-pyautogui.click()
+#pyautogui.displayMousePosition()
+#quit()
 
 #set this count to however many videos you wanna delete off the list
 count = 4000
 for videos in range(count):
     try:
-        #don't forget to change both of these sets of numbers too!
+        #don't forget to change both of these sets of numbers
+        #where the hamburger menu is
         pyautogui.moveTo(1396, 377)
         time.sleep(.5)
         pyautogui.click()
+        #where the remove from watch later button is
         pyautogui.moveTo(1396, 500)
         time.sleep(.5)
         pyautogui.click()
